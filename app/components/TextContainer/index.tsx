328d3c65acee61797b8fd73543d73a84c853f5d0
Milton Carlos Katoo <mckatoo@gmail.com>
2022-05-14 20:09:58 -0300

    ✨ (feat): TextContainer to apresent texts with tags html

---
+++ app/components/TextContainer/index.tsx
@@ -0,0 +1,21 @@
+type BodyContainerProps = {
+  title: string
+  children: React.ReactNode
+}
+
+export const TextContainer = ({ title, children }: BodyContainerProps) => (
+  <div className="flex flex-col ml-52 mt-4">
+    <div className="font-indie_flower text-slate-700 font-bold before:content-['<html>'] after:content-['</html>']">
+      <div className="before:content-['<body>'] after:content-['</body>'] ml-4">
+        <header className="before:content-['<header>'] after:content-['</header>'] ml-4 mb-4 flex flex-col">
+          <span aria-label={title} className="ml-4 text-5xl font-body text-mck_aqua">{title}</span>
+        </header>
+        <main className="before:content-['<main>'] after:content-['</main>'] ml-4">
+          <div className="ml-4 text-gray-200 mb-2 font-body font-normal">
+            {children}
+          </div>
+        </main>
+      </div>
+    </div>
+  </div>
+)
