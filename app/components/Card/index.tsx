644896e64ca0b3ed81340ccbed27d5276d10c8f1
Milton Carlos Katoo <mckatoo@gmail.com>
2022-05-17 17:50:44 -0300

    ✨ (feat): Card component

---
+++ app/components/Card/index.tsx
@@ -0,0 +1,15 @@
+export type CardProps = {
+  title: string
+  subTitle: string
+  content: string
+}
+
+const Card = ({ title, subTitle, content }: CardProps) => (
+  <div className="w-60 bg-slate-700 rounded-md flex flex-col gap-2 p-2">
+    <h1 className='text-slate-100 text-lg'>{title}</h1>
+    <h2 className='text-xs font-medium italic'>{subTitle}</h2>
+    <div className='overflow-clip'>{content}</div>
+  </div>
+)
+
+export default Card
