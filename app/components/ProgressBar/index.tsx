a955c8402dad872abcd049eba3050fc5aad465a2
Milton Carlos Katoo <mckatoo@gmail.com>
2022-05-17 17:01:49 -0300

    ✨ (feat): Progress Bar component with label and percent

---
+++ app/components/ProgressBar/index.tsx
@@ -0,0 +1,15 @@
+export type ProgressBarProps = {
+  label: string
+  percent: number
+}
+
+const ProgressBar = ({ label, percent }: ProgressBarProps) => (
+  <div className="flex flex-col gap-1">
+    <span>{label}</span>
+    <div className='h-px bg-slate-600'>
+      <div className='h-full bg-mck_aqua' style={{ width: `${percent}%` }} />
+    </div>
+  </div>
+)
+
+export default ProgressBar
