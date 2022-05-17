7cd7cc832fd988722e5d972013a05191ca620b57
Milton Carlos Katoo <mckatoo@gmail.com>
2022-05-17 19:59:13 -0300

    ♻️ (refact): Change title page

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -41,7 +41,7 @@ export const loader: LoaderFunction = async ({ params }) => {
 }
 
 export const meta: MetaFunction = () => ({
-  title: "iKatoo - Milton Carlos Katoo - Desenvolvedor Web"
+  title: "Milton Carlos Katoo - Sobre"
 })
 
 export default () => {
