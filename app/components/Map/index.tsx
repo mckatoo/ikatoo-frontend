5e3bb8ba4eee6856cf62dd4398c69ad80a788f3d
Milton Carlos Katoo <mckatoo@gmail.com>
2022-05-18 18:35:25 -0300

    ✨ (feature): Map component for use on contact page

---
+++ app/components/Map/index.tsx
@@ -0,0 +1,36 @@
+import { secrets } from 'secrets'
+
+import { GoogleMap, Marker, useJsApiLoader } from '@react-google-maps/api'
+
+export type LocalizationType = {
+  lat: number
+  lng: number
+}
+
+export type LabelMarkerOptions = google.maps.MarkerLabel
+
+export type MapProps = {
+  center: LocalizationType
+  label?: LabelMarkerOptions
+}
+
+
+const Map = ({ center, label }: MapProps) => {
+
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: secrets.GOOGLE_MAPS_API_KEY
+  })
+
+  return isLoaded ? (
+    <GoogleMap
+      mapContainerStyle={{ height: '100%', width: '100%' }}
+      center={center}
+      zoom={15}
+    >
+      <Marker position={center} options={{ label }} />
+    </GoogleMap>
+  ) : <></>
+}
+
+export default Map
