90863861df667c69b1a82edcd15737753a56d01c
Milton Carlos Katoo <mckatoo@gmail.com>
2022-06-24 19:38:40 -0300

    Nested routes on public pages

--- app/services/api.ts
+++ app/services/api.ts
@@ -1,9 +1,11 @@
 import axios from "axios";
 
-const API_URL = process.env.REACT_APP_API_URL;
+const API_URL = process.env.API_URL;
 
 const api = axios.create({
   baseURL: API_URL,
 });
+
+console.log('api', api)
 
 export default api;
