249621250c7fc46251956c44a971791eb333b45b
Milton Carlos Katoo <mckatoo@gmail.com>
2022-06-25 20:40:16 -0300

    Get menu data from api

--- app/routes/admin.tsx
+++ app/routes/admin.tsx
@@ -3,25 +3,29 @@ import { json } from "@remix-run/node";
 import { Outlet, useLoaderData } from "@remix-run/react";
 import type { SideMenuProps } from "~/components/SideMenu";
 import SideMenu from "~/components/SideMenu";
+import api from "../services/api";
 
 type LoaderData = { sideMenuData: SideMenuProps };
 
 export const loader: LoaderFunction = async ({ request }) => {
-  const sideMenuData: SideMenuProps = {
-    links: [
-      { label: "Sobre", to: "about" },
-      { label: "Habilidades", to: "skills" },
-      { label: "Projetos", to: "projects" },
-      { label: "Contatos", to: "contact" }
-    ],
-    social: {
-      linkedin: "mckatoo",
-      github: "mckatoo",
-      youtube: "UCc1e1mclC9o5OnQU87PcU1g",
-    }
-  }
+  const {data} = await api.post('/menu', { menu: 'admin' })
 
-  return json<LoaderData>({ sideMenuData });
+  return json<LoaderData>({ sideMenuData: data })
+  // const sideMenuData: SideMenuProps = {
+  //   links: [
+  //     { label: "Sobre", to: "about" },
+  //     { label: "Habilidades", to: "skills" },
+  //     { label: "Projetos", to: "projects" },
+  //     { label: "Contatos", to: "contact" }
+  //   ],
+  //   social: {
+  //     linkedin: "mckatoo",
+  //     github: "mckatoo",
+  //     youtube: "UCc1e1mclC9o5OnQU87PcU1g",
+  //   }
+  // }
+
+  // return json<LoaderData>({ sideMenuData });
 };
 
 export default () => {
