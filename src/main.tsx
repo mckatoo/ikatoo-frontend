d3d21063029f626ec996cb595aa76adb1de09e86
Milton Carlos Katoo <mckatoo@gmail.com>
2022-10-13 18:42:21 -0300

    github auth redirect

--- src/main.tsx
+++ src/main.tsx
@@ -3,20 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import "./index.css"
 
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <App />,
-//   },
-//   {
-//     path: "about",
-//     element: <About />,
-//   },
-// ]);
-
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    {/* <RouterProvider router={router} /> */}
-    <App />
-  </React.StrictMode>
+  <App />
 )
