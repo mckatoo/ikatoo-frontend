1642bc628f3dd6939606a044824382f9b2948933
Milton Carlos Katoo <mckatoo@gmail.com>
2022-10-14 10:56:21 -0300

    remove unused imports and define scope

--- src/services/github/auth.ts
+++ src/services/github/auth.ts
@@ -2,13 +2,12 @@ const auth = () => {
   const GITHUB_AUTH_URL = 'https://github.com/login/oauth/authorize'
   const params = new URLSearchParams([
     ['response_type', 'code'],
-    ['scope', 'user'],
+    ['scope', 'user public_repo'],
     ['client_id', `${import.meta.env.VITE_GITHUB_CLIENT_ID}`],
     ['state', 'test-t5']
   ]).toString()
   const url = `${GITHUB_AUTH_URL}?${params}`
   window.location.assign(url)
-  // return url
 }
 
 export default auth
