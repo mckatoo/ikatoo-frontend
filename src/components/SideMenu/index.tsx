62d6179c5b9a68c2d81ebee27f445b0fb74585a5
Milton Carlos Katoo <mckatoo@gmail.com>
2022-10-15 12:16:22 -0300

    should activate the item when the route is root

--- src/components/SideMenu/index.tsx
+++ src/components/SideMenu/index.tsx
@@ -18,6 +18,12 @@ export type SideMenuProps = {
 }
 
 const SideMenu = ({ social, links }: SideMenuProps) => {
+
+  const getStyle = (to: string, isActive: boolean) => {
+    const isRoot = to === 'about' && window.location.pathname === '/'
+    return isActive || isRoot ? 'text-mck_aqua' : 'text-gray-500'
+  }
+
   return (
     <>
       <menu
@@ -41,7 +47,7 @@ const SideMenu = ({ social, links }: SideMenuProps) => {
           <ul className="list-reset border-slate-700 border-y-2 divide-y divide-slate-700 font-semibold">
             {!!links.length && links.map(({ label, to }, index) => (
               <li key={index} className="py-4 px-10 w-full">
-                <NavLink to={to} className={({ isActive }) => isActive ? 'text-mck_aqua' : 'text-gray-500'}>
+                <NavLink to={to} className={({ isActive }) => getStyle(to, isActive)}>
                   {label}
                 </NavLink>
               </li>
