3aa030acb6e31f55dcb0e827d7e09b9b691c7d24
Milton Carlos Katoo <mckatoo@gmail.com>
2022-10-16 13:59:21 -0300

    login with github

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,51 @@
+import { useEffect, useState } from "react";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Alert from "./components/Alert";
 import { About } from "./pages/About";
 import { Contact } from "./pages/Contact";
 import { Login } from "./pages/Login";
 import { Projects } from "./pages/Projects";
 import { Skills } from "./pages/Skills";
 import { PublicLayout } from "./PublicLayout";
+import api from "./services/api";
 
 function App() {
+  const [error, setError] = useState('');
+  const [user, setUser] = useState({});
+
+  const githubAuth = async (code: string) => {
+    try {
+      const { data } = await api.post('auth/github', { code })
+      setUser(data)
+    } catch (error) {
+      if (error instanceof Error)
+        setError(error.message)
+    }
+  }
+
+  useEffect(() => {
+    const queryString = window.location.search
+    const urlParams = new URLSearchParams(queryString)
+    const code = urlParams.get("code")
+    !!code && githubAuth(code)
+  }, []);
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<PublicLayout />}>
-          <Route index element={<Navigate replace to="/about" />} />
-          <Route path="about" element={<About />} />
-          <Route path="skills" element={<Skills />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="projects" element={<Projects />} />
-        </Route>
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      {!!error && <Alert title={error} type={"error"} />}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<PublicLayout />}>
+            <Route index element={<About />} />
+            <Route path="about" element={<About />} />
+            <Route path="skills" element={<Skills />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="projects" element={<Projects />} />
+          </Route>
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,106 +1,99 @@
 import Logo from "../components/Logo";
 import auth from "../services/github/auth";
 
-export const Login = () => {
-  // const loginWithGithub = () => {
-  //   window.location.assign(auth())
-  // }
+export const Login = () =>
+  <section className="h-screen w-screen">
+    <div className="container w-96 mx-auto px-6 py-12 h-full">
+      <div className="flex justify-center items-center flex-wrap h-full g-6 text-gray-800">
+        <div className="text-gray-300">
+          <Logo name="Milton Carlos Katoo" description="Software Developer" />
+        </div>
+        <div className="w-full">
+          <form>
+            <div className="mb-6">
+              <input
+                type="text"
+                className="form-control block w-full px-4 py-2 font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                placeholder="Email address"
+              />
+            </div>
 
-  return (
-    <section className="h-screen w-screen">
-      <div className="container w-96 mx-auto px-6 py-12 h-full">
-        <div className="flex justify-center items-center flex-wrap h-full g-6 text-gray-800">
-          <div className="text-gray-300">
-            <Logo name="Milton Carlos Katoo" description="Software Developer" />
-          </div>
-          <div className="w-full">
-            <form>
-              <div className="mb-6">
+            <div className="mb-6">
+              <input
+                type="password"
+                className="form-control block w-full px-4 py-2 font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                placeholder="Password"
+              />
+            </div>
+
+            <div className="flex justify-between items-center mb-6">
+              <div className="form-group form-check">
                 <input
-                  type="text"
-                  className="form-control block w-full px-4 py-2 font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-                  placeholder="Email address"
+                  type="checkbox"
+                  name="remember"
+                  id="remember"
+                  className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+                  defaultChecked
                 />
-              </div>
 
-              <div className="mb-6">
-                <input
-                  type="password"
-                  className="form-control block w-full px-4 py-2 font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-                  placeholder="Password"
-                />
-              </div>
-
-              <div className="flex justify-between items-center mb-6">
-                <div className="form-group form-check">
-                  <input
-                    type="checkbox"
-                    name="remember"
-                    id="remember"
-                    className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
-                    defaultChecked
-                  />
-
-                  <label className="form-check-label inline-block text-gray-400" htmlFor="remember"
-                  >Remember me</label
-                  >
-                </div>
-                <a
-                  href="#!"
-                  className="text-gray-500 hover:text-gray-400 focus:text-gray-600 active:text-gray-800 duration-200 transition ease-in-out"
-                >Forgot password?</a
+                <label className="form-check-label inline-block text-gray-400" htmlFor="remember"
+                >Remember me</label
                 >
               </div>
-
-              <button
-                type="submit"
-                className="inline-block px-7 py-3 text-mck_aqua bg-slate-800 focus:outline-none hover:bg-mck_aqua hover:text-slate-800 font-medium text-sm leading-snug uppercase rounded shadow-md hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out w-full"
-                data-mdb-ripple="true"
-                data-mdb-ripple-color="light"
+              <a
+                href="#!"
+                className="text-gray-500 hover:text-gray-400 focus:text-gray-600 active:text-gray-800 duration-200 transition ease-in-out"
+              >Forgot password?</a
               >
-                Sign in
-              </button>
-            </form>
-            <div
-              className="flex items-center my-4 before:flex-1 before:border-t before:border-gray-300 before:mt-0.5 after:flex-1 after:border-t after:border-gray-300 after:mt-0.5"
-            >
-              <p className="text-center font-semibold mx-4 mb-0">OR</p>
             </div>
 
-
-            <div>
-              {/* {error ? <div>{error.message}</div> : null} */}
-              <button
-                className="px-7 py-3 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full flex justify-center items-center mb-3"
-                style={{ backgroundColor: "#3b5998" }}
-                data-mdb-ripple="true"
-                data-mdb-ripple-color="light"
-                onClick={auth}
-              >
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-github mr-2" viewBox="0 0 16 16">
-                  <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
-                </svg>
-                Continue with GitHub
-              </button>
-
-            </div>
-            <a
-              className="px-7 py-3 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full flex justify-center items-center"
-              style={{ backgroundColor: "#55acee" }}
-              href="#!"
-              role="button"
+            <button
+              type="submit"
+              className="inline-block px-7 py-3 text-mck_aqua bg-slate-800 focus:outline-none hover:bg-mck_aqua hover:text-slate-800 font-medium text-sm leading-snug uppercase rounded shadow-md hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out w-full"
               data-mdb-ripple="true"
               data-mdb-ripple-color="light"
             >
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
-                <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
-                <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
-              </svg>
-              Continue with Email
-            </a>
+              Sign in
+            </button>
+          </form>
+          <div
+            className="flex items-center my-4 before:flex-1 before:border-t before:border-gray-300 before:mt-0.5 after:flex-1 after:border-t after:border-gray-300 after:mt-0.5"
+          >
+            <p className="text-center font-semibold mx-4 mb-0">OR</p>
           </div>
+
+
+          <div>
+            {/* {error ? <div>{error.message}</div> : null} */}
+            <button
+              className="px-7 py-3 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full flex justify-center items-center mb-3"
+              style={{ backgroundColor: "#3b5998" }}
+              data-mdb-ripple="true"
+              data-mdb-ripple-color="light"
+              onClick={auth}
+            >
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-github mr-2" viewBox="0 0 16 16">
+                <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
+              </svg>
+              Continue with GitHub
+            </button>
+
+          </div>
+          <a
+            className="px-7 py-3 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full flex justify-center items-center"
+            style={{ backgroundColor: "#55acee" }}
+            href="#!"
+            role="button"
+            data-mdb-ripple="true"
+            data-mdb-ripple-color="light"
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
+              <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
+              <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
+            </svg>
+            Continue with Email
+          </a>
         </div>
       </div>
-    </section >
-  );
-}
+    </div>
+  </section >
