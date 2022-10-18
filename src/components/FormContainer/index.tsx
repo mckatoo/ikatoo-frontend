6f4d1426799c044355f4e98075a6851e9dbcf27c
Milton Carlos Katoo <mckatoo@gmail.com>
2022-10-18 20:18:42 -0300

    form wrapper

---
+++ src/components/FormContainer/index.tsx
@@ -0,0 +1,13 @@
+import { FormWrapper, Wrapper } from "./styles"
+
+type BodyContainerProps = {
+  children: React.ReactNode
+}
+
+export const FormContainer = ({ children }: BodyContainerProps) => (
+  <Wrapper>
+    <FormWrapper>
+      {children}
+    </FormWrapper>
+  </Wrapper>
+)
---
+++ src/components/FormContainer/styles.ts
@@ -0,0 +1,18 @@
+import tw from "tailwind-styled-components";
+
+export const Wrapper = tw.div`
+  mt-2
+  font-indie_flower
+  text-slate-700
+  font-bold
+  before:content-['<form>']
+  after:content-['</form>']
+`
+
+export const FormWrapper = tw.div`
+  ml-4
+  text-gray-200
+  mb-2
+  font-body
+  font-normal
+`
