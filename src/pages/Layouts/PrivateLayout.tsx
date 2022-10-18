7894edc44ee0bc1e50cfc921a9520d2cf6b39a4c
Milton Carlos Katoo <mckatoo@gmail.com>
2022-10-18 20:15:03 -0300

    move pages layout

---
+++ src/pages/Layouts/PrivateLayout.tsx
@@ -0,0 +1,51 @@
+import { Outlet } from "react-router-dom"
+import SideMenu, { SideMenuProps } from "../../components/SideMenu"
+
+export const PrivateLayout = () => {
+  const menu: SideMenuProps = {
+    links: [
+      {
+        label: 'Sobre',
+        to: 'about'
+      },
+      {
+        label: 'Habilidades',
+        to: 'skills'
+      },
+      {
+        label: 'Projetos',
+        to: 'projects'
+      },
+      {
+        label: 'Contato',
+        to: 'contact'
+      },
+    ],
+    social: [
+      {
+        name: "github",
+        url: "https://github.com/mckatoo",
+        url_icon: "/images/github.svg"
+      },
+      {
+        name: "youtube",
+        url: "https://youtube.com/mckatoo",
+        url_icon: "/images/youtube.svg"
+      },
+      {
+        name: "linkedin",
+        url: "https://linkedin.com/mckatoo",
+        url_icon: "/images/linkedin.svg"
+      },
+    ]
+  }
+
+  return (
+    <div className="h-screen flex flex-cols">
+      <SideMenu {...menu} />
+      <div className="h-screen overflow-y-auto">
+        <Outlet />
+      </div>
+    </div>
+  )
+}
