22c8092c8fe032cebb372f8c134739d4d066739b
Milton Carlos Katoo <mckatoo@gmail.com>
2022-10-26 12:24:36 -0300

    remove unnecessary functions

--- src/pages/Admin/AdminAbout.tsx
+++ src/pages/Admin/AdminAbout.tsx
@@ -1,27 +1,19 @@
-import Alert from "../../components/Alert";
 import { FormContainer } from "../../components/FormContainer";
-import Session, { SessionProps } from "../../components/Session";
 import { TextContainer } from "../../components/TextContainer";
 
 export const AdminAbout = () => {
 
-  const user: SessionProps = {
-    avatar: {
-      url: 'https://avatars.githubusercontent.com/u/7876852?s=40&v=4',
-      alt: 'Milton and the daughter.'
-    }
-  }
+  return (
+    <div className="flex flex-row bg-mck_black_light min-h-screen h-full w-full">
+      <div className='flex flex-row items-start ml-4 mt-4'>
+        <div className='flex flex-row w-1/2'>
+          <TextContainer title={'Informações sobre você.'}>
+            <FormContainer>
 
-  return <div className="flex flex-row bg-mck_black_light min-h-screen h-full w-full">
-    <Session {...user} />
-    <div className='flex flex-row items-start ml-4 mt-4'>
-      <div className='flex flex-row w-1/2'>
-        <TextContainer title={'Informações sobre você.'}>
-          <FormContainer>
-
-          </FormContainer>
-        </TextContainer>
+            </FormContainer>
+          </TextContainer>
+        </div>
       </div>
     </div>
-  </div>
+  )
 }
--- src/pages/Admin/AdminSkills.tsx
+++ src/pages/Admin/AdminSkills.tsx
@@ -1,37 +1,19 @@
-import { useEffect, useState } from "react";
-import Alert from "../../components/Alert";
-import Session, { SessionProps } from "../../components/Session";
-import api from "../../services/api";
+import { FormContainer } from "../../components/FormContainer";
+import { TextContainer } from "../../components/TextContainer";
 
 export const AdminSkills = () => {
-  const [error, setError] = useState('');
 
-  const githubAuth = async (code: string) => {
-    try {
-      const { data } = await api.post('auth/github', { code })
-    } catch (error) {
-      if (error instanceof Error)
-        setError(error.message)
-    }
-  }
+  return (
+    <div className="flex flex-row bg-mck_black_light min-h-screen h-full w-full">
+      <div className='flex flex-row items-start ml-4 mt-4'>
+        <div className='flex flex-row w-1/2'>
+          <TextContainer title={'Informações sobre você.'}>
+            <FormContainer>
 
-  useEffect(() => {
-    const queryString = window.location.search
-    const urlParams = new URLSearchParams(queryString)
-    const code = urlParams.get("code")
-    !!code && githubAuth(code)
-  }, []);
-
-  const user: SessionProps = {
-    email: 'teste@teste.com',
-    avatar: {
-      url: 'https://avatars.githubusercontent.com/u/7876852?s=40&v=4',
-      alt: 'Milton and the daughter.'
-    }
-  }
-
-  return <>
-    {!!error && <Alert title={error} type={"error"} />}
-    <Session {...user} />
-  </>
+            </FormContainer>
+          </TextContainer>
+        </div>
+      </div>
+    </div>
+  )
 }
