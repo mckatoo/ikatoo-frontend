9de503ff6ba59ed66b8c9533c364c518afed9829
Milton Carlos Katoo <mckatoo@gmail.com>
2022-10-31 19:01:10 -0300

    setup vitest with testing-library

---
+++ src/components/TextInput/test.tsx
@@ -0,0 +1,12 @@
+import { render } from '@testing-library/react'
+import React from 'react'
+import { describe, expect, it } from 'vitest'
+import TextInput from '.'
+
+describe('TextInput', () => {
+  it('should be able to render the text input', () => {
+    const { getByRole } = render(<TextInput />)
+
+    expect(getByRole('textbox')).toBeInTheDocument()
+  })
+})
