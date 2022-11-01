e851c131ce8b5cbb16c7ed398def2de9a8027c6a
Milton Carlos Katoo <mckatoo@gmail.com>
2022-11-01 17:03:01 -0300

    render svg dynamically

--- src/components/SideMenu/index.tsx
+++ src/components/SideMenu/index.tsx
@@ -1,5 +1,5 @@
 import { NavLink } from 'react-router-dom'
-
+import SVG from 'react-inlinesvg'
 import Logo from '../Logo'
 
 type SideMenuItem = {
@@ -28,62 +28,61 @@ const SideMenu = ({ social, links }: SideMenuProps) => {
   }
 
   return (
-    <>
-      <menu
-        aria-label="Side Menu"
-        className="
+    <menu
+      aria-label="Side Menu"
+      className="
           text-center
           w-fit
           max-w-[13rem]
           z-20
           h-full
           bg-slate-800
+          text-gray-500
           flex
           flex-col
           justify-between
       "
-      >
-        <div className="z-30 flex justify-center px-4 py-8 bg-mck_black">
-          <Logo
-            aria-label="Logotipo"
-            name="Milton Carlos Katoo"
-            description="Software Developer"
-          />
-        </div>
+    >
+      <div className="z-30 flex justify-center px-4 py-8 bg-mck_black">
+        <Logo
+          aria-label="Logotipo"
+          name="Milton Carlos Katoo"
+          description="Software Developer"
+        />
+      </div>
 
-        <div className="text-lg">
-          <ul className="list-reset border-slate-700 border-y-2 divide-y divide-slate-700 font-semibold">
-            {!!links.length &&
-              links.map(({ label, to }, index) => (
-                <li key={index} className="py-4 px-10 w-full">
-                  <NavLink
-                    to={to}
-                    className={({ isActive }) => getStyle(to, isActive)}
-                  >
-                    {label}
-                  </NavLink>
-                </li>
-              ))}
-          </ul>
-        </div>
-
-        <div className="flex flex-row justify-around py-2">
-          {!!social.length &&
-            social.map((social, index) => (
-              <div key={index} className="w-6">
-                <a
-                  className="text-slate-100"
-                  href={social.url}
-                  target="_blank"
-                  rel="noreferrer"
+      <div className="text-lg">
+        <ul className="list-reset border-slate-700 border-y-2 divide-y divide-slate-700 font-semibold">
+          {!!links.length &&
+            links.map(({ label, to }, index) => (
+              <li key={index} className="py-4 px-10 w-full">
+                <NavLink
+                  to={to}
+                  className={({ isActive }) => getStyle(to, isActive)}
                 >
-                  <img src={social.url_icon} alt={social.name} />
-                </a>
-              </div>
+                  {label}
+                </NavLink>
+              </li>
             ))}
-        </div>
-      </menu>
-    </>
+        </ul>
+      </div>
+
+      <div className="flex flex-row justify-around py-2">
+        {!!social.length &&
+          social.map((social, index) => (
+            <div key={index} className="w-6">
+              <a
+                className="text-inherit"
+                href={social.url}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <SVG src={social.url_icon} title={social.name} />
+              </a>
+            </div>
+          ))}
+      </div>
+    </menu>
   )
 }
 
