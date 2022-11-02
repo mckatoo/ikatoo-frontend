5c8936ece4bfbbddb34e6d66c9ec8e80ce8f4c9d
Milton Carlos Katoo <mckatoo@gmail.com>
2022-11-01 22:41:37 -0300

    tests for desktop version

---
+++ src/components/Menu/mock.ts
@@ -0,0 +1,39 @@
+import { SideMenuProps } from '.'
+
+export const mockedMenu: SideMenuProps = {
+  links: [
+    {
+      label: 'Sobre',
+      to: 'about'
+    },
+    {
+      label: 'Habilidades',
+      to: 'skills'
+    },
+    {
+      label: 'Projetos',
+      to: 'projects'
+    },
+    {
+      label: 'Contato',
+      to: 'contact'
+    }
+  ],
+  social: [
+    {
+      name: 'github',
+      url: 'https://github.com/mckatoo',
+      url_icon: '/images/github.svg'
+    },
+    {
+      name: 'youtube',
+      url: 'https://youtube.com/mckatoo',
+      url_icon: '/images/youtube.svg'
+    },
+    {
+      name: 'linkedin',
+      url: 'https://linkedin.com/mckatoo',
+      url_icon: '/images/linkedin.svg'
+    }
+  ]
+}
