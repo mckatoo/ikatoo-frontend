e4ac886c4cb60186177806b45927440776ce0a2b
Milton Carlos Katoo <mckatoo@gmail.com>
2022-11-05 12:27:04 -0300

    splited styles on styled component

---
+++ src/components/Alert/styles.ts
@@ -0,0 +1,56 @@
+import tw from 'tailwind-styled-components'
+
+type Props = {
+  $isClosed?: boolean
+  $color?: string
+}
+
+const isClosed = (props: Props) => {
+  if (props.$isClosed) return `hidden`
+}
+
+export default {
+  Main: tw.div`
+    ${(props: Props) => isClosed(props)}
+    fixed
+    w-screen
+    z-50
+    font-semibold
+    ${(props: Props) => props.$color}
+    text-sm
+    shadow-lg
+  `,
+  Wrapper: tw.div`
+    flex
+    items-center
+    p-1
+  `,
+  Title: tw.span`
+    w-full
+    text-center
+  `,
+  Button: tw.button`
+    ml-auto
+    inline-flex
+    flex-shrink-0
+    justify-center
+    items-center
+    h-5
+    w-5
+    rounded-md
+    text-white/[.5]
+    hover:text-white
+    focus:outline-none
+    focus:ring-2
+    focus:ring-offset-2
+    focus:ring-offset-gray-800
+    focus:ring-gray-600
+    transition-all
+    text-sm
+    dark:focus:ring-offset-gray-900
+    dark:focus:ring-gray-800
+    ${(props: Props) => isClosed(props)}
+    ${(props: Props) => props.$color}
+    mr-4
+  `
+}
