dda051f7c20b3ea39e95bebcff60265db5b8548a
Milton Carlos Katoo <mckatoo@gmail.com>
2022-11-13 00:41:14 -0300

    fix lint on tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,26 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     fontFamily: {
-      'display': ['Oswald', 'Helvetica', 'Arial', 'sans-serif'],
-      'body': ['"Open Sans"', 'Helvetica', 'Arial', 'sans-serif'],
-      'indie_flower': ['Indie Flower', 'monospace']
+      display: ['Oswald', 'Helvetica', 'Arial', 'sans-serif'],
+      body: ['"Open Sans"', 'Helvetica', 'Arial', 'sans-serif'],
+      indie_flower: ['Indie Flower', 'monospace']
     },
     extend: {
       boxShadow: {
-        'image': 'inset 0px 0px 64px 47px #00032c'
+        image: 'inset 0px 0px 64px 47px #00032c'
       },
       colors: {
-        'mck_black': '#00021f',
-        'mck_black_light': '#00032c',
-        'mck_aqua': '#08FDD8',
-        'mck_red': '#ff2a2a',
-      },
-    },
+        mck_black: '#00021f',
+        mck_black_light: '#00032c',
+        mck_aqua: '#08FDD8',
+        mck_red: '#ff2a2a',
+        mck_gray_light: 'rgb(229 231 235)',
+        mck_gray_dark: 'rgb(31 41 55)'
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
