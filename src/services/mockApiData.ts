5315d40eaf917faf2b667c4855613335fc5414ee
Milton Carlos Katoo <mckatoo@gmail.com>
2022-11-13 00:29:44 -0300

    🤡 (test): mocked api data

---
+++ src/services/mockApiData.ts
@@ -0,0 +1,16 @@
+export default {
+  signIn: {
+    user: {
+      id: 'id0-fasdlfjhgsdf',
+      username: 'username-fasdflkhgsdf',
+      name: '90asdfsdfkjasf-name',
+      email: 'fasdj@aflsdf.com',
+      avatar: {
+        url: 'dfsadkkhhggsdf-url.com',
+        alt: 'dfsadkkhhggsdf-alt'
+      }
+    },
+    accessToken: 'asldfjlsdfjsd',
+    refreshToken: 'asldfjlsdfjsd'
+  }
+}
