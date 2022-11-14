9413b5d4d60bb1dccca6eb41f770d704357b97bf
Milton Carlos Katoo <mckatoo@gmail.com>
2022-11-14 12:02:22 -0300

    💄 (styles): change grid behavior

--- src/pages/Projects/styles.ts
+++ src/pages/Projects/styles.ts
@@ -4,10 +4,11 @@ export default {
   Wrapper: tw.div`
     grid
     grid-cols-1
-    md:grid-cols-2
+    md:grid-cols-3
     p-4
     items-center
     gap-2
+    md:gap-4
   `,
   CardWrapper: tw.div`
     h-full
