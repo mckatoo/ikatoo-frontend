748c253b09c439dadbdbc8b39c133019c8856416
Milton Carlos Katoo <mckatoo@gmail.com>
2022-11-15 20:18:54 -0300

    refactory component for use styled component only

--- src/components/TextInput/stories.tsx
+++ src/components/TextInput/stories.tsx
@@ -12,13 +12,20 @@ export default {
     initialValue: '',
     placeholder: 'john.cage@gmail.com',
     disabled: false
-  },
-  decorators: [(Styles) => <div className="max-w-xs p-4">{Styles()}</div>]
+  }
 } as ComponentMeta<typeof TextInput>
 
-export const Default = {} as ComponentStory<typeof TextInput>
-export const WithError = {
-  args: {
-    error: 'Ops...something is wrong'
-  }
-} as ComponentStory<typeof TextInput>
+export const Default: ComponentStory<typeof TextInput> = (args) => (
+  <div style={{ maxWidth: 300, padding: 15 }}>
+    <TextInput {...args} />
+  </div>
+)
+
+export const withError: ComponentStory<typeof TextInput> = (args) => (
+  <div style={{ maxWidth: 300, padding: 15 }}>
+    <TextInput {...args} />
+  </div>
+)
+withError.args = {
+  error: 'Ops...something is wrong'
+}
--- src/components/TextInput/styles.ts
+++ src/components/TextInput/styles.ts
@@ -1,98 +1,116 @@
-import styled from 'styled-components'
-import tw from 'tailwind-styled-components'
+import styled, { css } from 'styled-components'
 
-type IconPositionProps = {
-  $iconPosition?: 'left' | 'right'
-}
+import { TextInputProps } from '.'
+import theme from '../../theme'
 
-type WrapperProps = {
-  $error?: boolean
-  $disabled?: boolean
-}
+type IconPositionProps = Pick<TextInputProps, 'iconPosition'>
 
-const Icon = styled.div`
-  & > svg {
-    width: 2.2rem;
-    height: 100%;
-  }
+type WrapperProps = Pick<TextInputProps, 'disabled'> & { error?: boolean }
+
+const InputWrapper = styled.div`
+  ${() => css`
+    display: flex;
+    align-items: center;
+    background: ${theme.colors.lightGray};
+    border-radius: 0.2rem;
+    padding: 0 ${theme.spacings.xsmall};
+    border: 0.2rem solid;
+    border-color: ${theme.colors.lightGray};
+
+    &:focus-within {
+      box-shadow: 0 0 0.5rem ${theme.colors.primary};
+    }
+  `}
 `
 
-const Styles = {
-  InputWrapper: tw.div`
-    flex
-    items-center
-    bg-mck_gray_light
-    rounded-md
-    p-1
-    border-solid
-    border-2
-    border-mck_gray_light
-    focus-within:shadow-sm
-    focus-within:bg-mck_primary
-    focus-within:text-mck_gray_light
-  `,
-  Input: tw.input`
-    text-mck_black
-    focus:text-mck_gray_light
-    font-display
-    text-base
-    p-1
-    ${(props: { $iconPosition?: 'left' | 'right' }) =>
-      !!props.$iconPosition &&
-      (props.$iconPosition === 'left' ? 'pl-3' : 'pr-3')}
-    bg-transparent
-    border-0
-    outline-none
+const Input = styled.input<IconPositionProps>`
+  ${({ iconPosition }) => css`
+    color: ${theme.colors.black};
+    font-family: ${theme.font.family};
+    font-size: ${theme.font.sizes.large};
+    padding: ${theme.spacings.xsmall} 0;
+    padding-${iconPosition}: ${theme.spacings.xsmall};
+    background: transparent;
+    border: 0;
+    outline: none;
+    width: ${iconPosition === 'right' ? `calc(100% - 2.2rem)` : `100%`};
+    &:-webkit-autofill {
+      -webkit-box-shadow: 0 0 0 ${theme.spacings.small}
+        ${theme.colors.lightGray} inset;
+      filter: none;
+      &::first-line {
+        font-family: ${theme.font.family};
+        font-size: ${theme.font.sizes.large};
+      }
+    }
+  `}
+`
 
-    autofill:shadow-inner
-    autofill:shadow-mck_gray_light
-    ${(props: IconPositionProps) =>
-      props.$iconPosition === 'right' ? 'w-[calc(100% - 2.2rem)]' : 'w-full'}
+const Label = styled.label`
+  ${() => css`
+    font-size: ${theme.font.sizes.small};
+    color: ${theme.colors.black};
+    cursor: pointer;
+  `}
+`
 
-    autofill:filter-none
-    autofill:first-line:font-display
-    autofill:first-line:text-base
-  `,
-  Label: tw.label`
-    text-base
-    text-mck_black
-    dark:text-mck_gray_light
-    cursor-pointer
-  `,
-  Icon: tw(Icon)`
-    flex
-    text-gray-500
-    ${(props: IconPositionProps) =>
-      props.$iconPosition === 'right' ? 'order-1' : 'order-none'}
-  `,
-  Error: tw.p`
-    text-mck_red
-    text-base
-  `,
-  Wrapper: tw.div`
-  ${(props: WrapperProps) => props.$error && wrapperModifiers.error()}
-  ${(props: WrapperProps) => props.$disabled && wrapperModifiers.disabled()}
-  `
-}
+const Icon = styled.div<IconPositionProps>`
+  ${({ iconPosition }) => css`
+    display: flex;
+    color: ${theme.colors.gray};
+    order: ${iconPosition === 'right' ? 1 : 0};
 
-const disabledStyle = `
-  cursor-not-allowed
-  text-gray-500
-  placeholder:text-current
+    & > svg {
+      width: 1.2rem;
+      height: 100%;
+    }
+  `}
+`
+
+const Error = styled.p`
+  ${() => css`
+    color: ${theme.colors.red};
+    font-size: ${theme.font.sizes.xsmall};
+  `}
 `
 
 const wrapperModifiers = {
-  error: () => `
-    ${(Styles.InputWrapper = tw(Styles.InputWrapper)`border-mck_red`)}
+  error: () => css`
+    ${InputWrapper} {
+      border-color: ${theme.colors.red};
+    }
 
-    ${(Styles.Icon = tw(Styles.Icon)`text-mck_red`)}
-    ${(Styles.Label = tw(Styles.Label)`text-mck_red`)}
-    `,
-  disabled: () => `
-    ${(Styles.Label = tw(Styles.Label)`${disabledStyle}`)}
-    ${(Styles.Input = tw(Styles.Input)`${disabledStyle}`)}
-    ${(Styles.Icon = tw(Styles.Icon)`${disabledStyle}`)}
+    ${Icon},
+    ${Label} {
+      color: ${theme.colors.red};
+    }
+  `,
+  disabled: () => css`
+    ${Label},
+    ${Input},
+    ${Icon} {
+      cursor: not-allowed;
+      color: ${theme.colors.gray};
+
+      &::placeholder {
+        color: currentColor;
+      }
+    }
   `
 }
 
-export default Styles
+const Wrapper = styled.div<WrapperProps>`
+  ${({ error, disabled }) => css`
+    ${error && wrapperModifiers.error()}
+    ${disabled && wrapperModifiers.disabled()}
+  `}
+`
+
+export default {
+  Wrapper,
+  Error,
+  Icon,
+  Label,
+  Input,
+  InputWrapper
+}
