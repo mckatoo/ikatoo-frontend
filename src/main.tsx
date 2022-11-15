0dbe53e5818c3e5a35b53698c4262dce3f9aaa2d
Milton Carlos Katoo <mckatoo@gmail.com>
2022-11-15 10:27:44 -0300

    implemented error bondary

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,18 @@ import './index.css'
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ErrorBoundary } from 'react-error-boundary'
 
 import App from './App'
+import ErrorFallback from './ErrorFallback'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary
+      FallbackComponent={ErrorFallback}
+      onError={(error, info) => console.log('error', error, 'info', info)}
+    >
+      <App />
+    </ErrorBoundary>
   </React.StrictMode>
 )
