82b7b07d2e741abcf82135aa88bcae09afee5621
Milton Carlos Katoo <mckatoo@gmail.com>
2023-01-10 16:39:51 -0300

    implicited env vars

--- src/helpers/env.ts
+++ src/helpers/env.ts
@@ -1,5 +1,10 @@
-export const env = (key: string) => {
+const env = (key: string) => {
   if (import.meta.env[key] === undefined)
     console.error(`*** ${key} IS UNDEFINED. ***`)
   return `${import.meta.env[key] ?? ''}`
+}
+
+export default {
+  VITE_GITHUB_CLIENT_ID: env('VITE_GITHUB_CLIENT_ID'),
+  VITE_REDIRECT_URL: env('VITE_REDIRECT_URL')
 }
