65eedbb41107a1fb7a934194275ec395efbcdd26
Milton Carlos Katoo <mckatoo@gmail.com>
2023-01-23 17:16:24 -0300

    init test for services

---
+++ src/services/aboutService/aboutService.test.ts
@@ -0,0 +1,9 @@
+import { describe, expect, test, vi } from 'vitest'
+
+vi.mock('axios')
+
+describe('About page fetch data', () => {
+  test('should create about page data', async () => {
+    expect(true).toBe(true)
+  })
+})
