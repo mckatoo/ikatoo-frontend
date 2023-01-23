d623683746ed657838d917308a9869fd7c18f0d5
Milton Carlos Katoo <mckatoo@gmail.com>
2023-01-22 23:24:16 -0300

    lint tests snapshots

---
+++ src/services/aboutService/aboutPageMock.ts
@@ -0,0 +1,12 @@
+export const aboutPageMock = {
+  title: 'About page title',
+  description: 'About page desc',
+  skills: [
+    { title: 'docker' },
+    { title: 'jest' },
+    { title: 'tdd' },
+    { title: 'vitest' }
+  ],
+  avatarURL: 'url of the avatar',
+  avatarALT: 'this is a avatar'
+}
