1e147abed1c62d3233fac7299e40cc7714b29816
Milton Carlos Katoo <mckatoo@gmail.com>
2023-01-27 12:40:20 -0300

    wait for render elements before expect

--- src/components/Menu/Mobile/test.tsx
+++ src/components/Menu/Mobile/test.tsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react'
+import { render, waitFor } from '@testing-library/react'
 import { MemoryRouter } from 'react-router-dom'
 import { describe, it } from 'vitest'
 import MobileMenu from '.'
@@ -6,18 +6,23 @@ import MobileMenu from '.'
 import { mockedMenu } from '../mock'
 
 describe('Menu Component', () => {
-  it('should render menu on desktop', () => {
+  it('should render menu on desktop', async () => {
     const { getByText, getByLabelText, container } = render(
       <MemoryRouter initialEntries={[`/`]}>
         <MobileMenu links={mockedMenu.links} social={mockedMenu.social} />
       </MemoryRouter>
     )
 
-    expect(getByLabelText(/mobile menu/i)).toBeInTheDocument()
-    expect(getByLabelText(/logotipo/i)).toBeInTheDocument()
-    expect(getByText(/sobre/i)).toHaveClass('text-mck_aqua')
-    expect(getByText(/habilidades/i)).toHaveClass('text-gray-500')
-    expect(getByText(/habilidades/i)).not.toHaveClass('text-mck_aqua')
+    const mobileMenu = await waitFor(() => getByLabelText(/mobile menu/i))
+    const logotipo = await waitFor(() => getByLabelText(/logotipo/i))
+    const sobre = await waitFor(() => getByText(/sobre/i))
+    const habilidades = await waitFor(() => getByText(/habilidades/i))
+
+    expect(mobileMenu).toBeInTheDocument()
+    expect(logotipo).toBeInTheDocument()
+    expect(sobre).toHaveClass('text-mck_aqua')
+    expect(habilidades).toHaveClass('text-gray-500')
+    expect(habilidades).not.toHaveClass('text-mck_aqua')
     expect(container).toMatchSnapshot()
   })
 })
