f6e0bf4c1d0f7234c9f6f7b4051f7113a083cc71
Milton Carlos Katoo <mckatoo@gmail.com>
2023-03-03 18:49:25 -0300

    add mock data from about page

---
+++ src/pages/About/mock.ts
@@ -0,0 +1,22 @@
+export const about = {
+  skills: [
+    'javascript',
+    'dotnet',
+    'nodedotjs',
+    'html5',
+    'css3',
+    'typescript',
+    'jest',
+    'json',
+    'git',
+    'bootstrap',
+    'react',
+    'mysql',
+    'npm',
+    'php',
+    'java'
+  ],
+  title: 'Olá. Bem vindo❗',
+  description:
+    '<p>Me chamo Milton Carlos Katoo, moro em Itapira, interior de São Paulo/Brasil. Pai de uma princesa e filho de excelente cozinheira Italiana e um saldoso Japonês faz tudo, sou um desenvolvedor full-stack que ama programação e desenvolvimento de software afim de melhorar a vida das pessoas.</p><p>Pessoa bem organizada, solucionador de problemas, funcionário independente com alta atenção aos detalhes. Gosto de animes, mangas, games, séries de TV e filmes. Pai orgulhoso de uma princesa, sou nascido em 1979 e sou innteressado em todo o espectro de programação.</p><a class="text-mck_aqua underline underline-offset-8" href="https://ikatoo.com.br/contact/" rel="contact"><span>🎉</span>Vamos fazer algo especial.</a><span>😄</span>'
+}
